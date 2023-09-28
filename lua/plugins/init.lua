181177f0deb58cd97ed534029b1c8885503a202f
Fergus Molloy <fergus@molloy.xyz>
2023-09-28 11:12:22 +0100

    update lazy packages

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -97,17 +97,14 @@ require('lazy').setup({
             require('bamboo').load()
         end,
     },
-
     {
         -- Add indentation guides even on blank lines
-        'lukas-reineke/indent-blankline.nvim',
+        "lukas-reineke/indent-blankline.nvim",
         event = "BufReadPost",
-        -- Enable `lukas-reineke/indent-blankline.nvim`
-        -- See `:help indent_blankline.txt`
+        main = "ibl",
         opts = {
-            char = '┊',
             show_trailing_blankline_indent = false,
-        },
+        }
     },
 
     -- "gc" to comment visual regions/lines
