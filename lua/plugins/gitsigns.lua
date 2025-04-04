b9ea7411e19accaa4a727ed48f0bafd7af922957
Fergus Molloy <fergus@molloy.xyz>
2025-04-04 11:31:27 +0100

    add keymaps for blame and preview hunk

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,11 @@
 return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
+    lazy = false,
+    keys = {
+      { '<leader>gb', '<cmd>Gitsigns blame<cr>', desc = 'Show blame for current buffer' },
+      { '<leader>gp', '<cmd>Gitsigns preview_hunk<cr>', desc = 'Preview hunk' },
+    },
     opts = {
       signs = {
         add = { text = '+' },
